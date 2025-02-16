3f3b056821236e6c66943b4ddca2c02af60e3bca
Muhammad Aqeel <104295133+Aqeel785@users.noreply.github.com>
2025-02-16 20:08:45 +0500

    skills and personal introduction

--- m.aqeel_portfolio/src/app/components/Skills.jsx
+++ m.aqeel_portfolio/src/app/components/Skills.jsx
@@ -1,69 +1,76 @@
-import React from "react"
+import React from "react";
 import {
   DiHtml5,
   DiCss3,
-  DiSass,
   DiBootstrap,
   DiJavascript1,
-  DiReact,
-  DiNodejsSmall,
-  DiMongodb,
   DiGithubBadge,
-} from "react-icons/di"
-import Reveal from "./Reveal"
+  DiPython, 
+} from "react-icons/di";
+import { 
+  SiDjango, SiFastapi, SiNextdotjs, SiNumpy, SiPandas, SiApachespark 
+} from "react-icons/si"; 
+import Reveal from "./Reveal";
 
 const skills = [
-    {
-      category: 'Frontend',
-      technologies: [
-        { name: 'HTML', icon: <DiHtml5 className='text-orange-600' /> },
-        { name: 'CSS', icon: <DiCss3 className='text-blue-600' /> },
-        { name: 'Sass', icon: <DiSass className='text-pink-600' /> },
-        { name: 'Bootstrap', icon: <DiBootstrap className='text-purple-600' /> },
-        { name: 'JavaScript', icon: <DiJavascript1 className='text-yellow-500' /> },
-        { name: 'React', icon: <DiReact className='text-blue-500' /> },
-      ],
-    },
-    {
-      category: 'Fullstack',
-      technologies: [
-        { name: 'Node Js', icon: <DiNodejsSmall className='text-green-500' /> },
-        { name: 'MongoDB', icon: <DiMongodb className='text-green-600' /> },
-        { name: 'React', icon: <DiReact className='text-blue-500' /> },
-        { name: 'GitHub', icon: <DiGithubBadge className='text-gray-600' /> },
-      ],
-    },
-  ]
+  // {
+  //   category: "Frontend",
+  //   technologies: [
+  //     { name: "HTML", icon: <DiHtml5 className="text-orange-600" /> },
+  //     { name: "CSS", icon: <DiCss3 className="text-blue-600" /> },
+  //     { name: "Bootstrap", icon: <DiBootstrap className="text-purple-600" /> },
+  //     { name: "JavaScript", icon: <DiJavascript1 className="text-yellow-500" /> },
+  //     { name: "Next.js", icon: <SiNextdotjs className="text-white" /> }, 
+  //   ],
+  // },
+  {
+    // category: "Backend",
+    technologies: [
+      { name: "Python", icon: <DiPython className="text-blue-400" /> },
+      { name: "Django", icon: <SiDjango className="text-green-500" /> },
+      { name: "GitHub", icon: <DiGithubBadge className="text-gray-600" /> },
+      { name: "Django Rest Framework", icon: <SiApachespark className="text-red-500" /> }, 
+      { name: "FastAPI", icon: <SiFastapi className="text-teal-500" /> },
+      { name: "NumPy", icon: <SiNumpy className="text-indigo-500" /> },  
+      { name: "Pandas", icon: <SiPandas className="text-red-500" /> },       
+    ],
+  },
+];
 
 const Skills = () => {
   return (
-    <div className="max-w-[650px] mx-auto flex flex-col justify-center px-4 text-gray-200 pb-8 md:py-12" id="skills">
+    <div className="flex justify-center items-center min-h-screen bg-gray-900 px-6 text-gray-200" id="skills">
+      <div className="max-w-[900px] w-full">
         <Reveal>
-        <h2 className="text-3xl font-bold mb-4 text-center">Skills</h2>
-        <p className="text-center mb-8">
-            I worked on various frontend and fullstack projects. Check them <a href="#" className="underline">there</a>.
-        </p>
+          <h2 className="text-6xl font-bold mb-6 text-center text-white">Skills</h2>
+          <p className="text-center mb-10 text-gray-100 text-xl">
+            I have worked on various  <strong>frontend</strong> and <strong>backend projects</strong>. Check them{" "}
+            <a href="https://github.com/MuhammadAqeel1209" className="underline text-blue-400 hover:text-blue-300">here</a>.
+          </p>
 
-        <div className="flex flex-col md:flex-row justify-center space-y-8 md:space-y-0 md:space-x-8
-                        ">
+          <div className="flex flex-col items-center">
             {skills.map((skill, index) => (
-                <div key={index} className="border border-purple-900 p-6 rounded-lg bg-purple-900/20 shadow-lg 
-                                w-full md:w-1/2">
-                    <h3 className="text-xl font-bold mb-4 text-center">{skill.category}</h3>
-                    <div className="grid grid-cols-2 gap-4">
-                        {skill.technologies.map((tech, idx) => (
-                            <div key={idx} className="flex items-center space-x-2">
-                                <span className="text-2xl">{tech.icon}</span>
-                                <span>{tech.name}</span>
-                            </div>
-                        ))}
+              <div
+                key={index}
+                className="border border-purple-800 p-6 rounded-xl bg-purple-900/20 shadow-lg 
+                          hover:shadow-purple-700/50 transition-shadow duration-300 w-[450px]"
+              >
+                <h3 className="text-3xl font-semibold mb-4 text-center text-white">{skill.category}</h3>
+                <div className="grid grid-cols-2 gap-4">
+                  {skill.technologies.map((tech, idx) => (
+                    <div key={idx} className="flex items-center space-x-3">
+                      <span className="text-3xl">{tech.icon}</span>
+                      <span className="text-lg text-gray-300">{tech.name}</span>
                     </div>
+                  ))}
                 </div>
-            ))}
-        </div>
+              </div>
+              ))}
+          </div>
         </Reveal>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
